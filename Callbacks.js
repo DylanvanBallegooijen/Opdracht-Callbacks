3d3b8b9686ac9f71957eece13853416c7238c480
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-11-01 10:51:30 +0100

    Add files via upload

---
+++ Callbacks.js
@@ -0,0 +1,12 @@
+
+
+function huiswerkMaken (subject){
+ alert("Ok, ok, ik ga nu mijn ${subject} maken");
+}
+
+huiswerkMaken('wiskunde');
+
+function klaarMetHuiswerk(){}
+alert("Kijk paps en mams, Ik ben klaar met mijn huiswerk");
+
+klaarMetHuiswerk();
